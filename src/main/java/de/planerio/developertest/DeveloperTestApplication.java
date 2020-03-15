9142aabfe59cd7ddc3eae031840db624529806b4
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2020-03-14 22:01:31 -0300

    refactor: adding improvements to setup Swagger

--- src/main/java/de/planerio/developertest/DeveloperTestApplication.java
+++ src/main/java/de/planerio/developertest/DeveloperTestApplication.java
@@ -3,11 +3,13 @@ package de.planerio.developertest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("de.planerio.developertest")
 @EntityScan("de.planerio.developertest")
 @SpringBootApplication
+@ComponentScan(basePackages = { "de.planerio.developertest", "de.planerio.developertest.controller" , "de.planerio.developertest.config"})
 public class DeveloperTestApplication {
 
 	public static void main(String[] args) {
