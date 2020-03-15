7bc8192f87f5ec1f776f12503b9eca0669f3e94a
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2020-03-14 21:32:27 -0300

    refactor: enhancing swagger annotation

--- src/main/java/de/planerio/developertest/config/SwaggerConfig.java
+++ src/main/java/de/planerio/developertest/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package de.planerio.developertest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,8 +14,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Rest Football")
+                .description("")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("Alesander Gonzaga","", "alesanderlg@gmail.com"))
+                .build();
+    }
+
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("de.planerio.developertest.controller"))
+                .build()
+                .apiInfo(apiInfo());
     }
 }
