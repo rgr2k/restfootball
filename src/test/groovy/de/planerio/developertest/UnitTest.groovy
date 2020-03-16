de335490c267c18506725895e4a1cc1a4d4976ae
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2020-03-16 11:32:34 -0300

    chore: changing build.gradle to apply groovy plugin

---
+++ src/test/groovy/de/planerio/developertest/UnitTest.groovy
@@ -0,0 +1,8 @@
+package de.planerio.developertest
+
+/**
+ * This is interface identifies all Groovy specification classes which contain unit tests.
+ */
+interface UnitTest {
+
+}
---
+++ src/test/groovy/de/planerio/developertest/service/CountryServiceTest.groovy
@@ -0,0 +1,12 @@
+package de.planerio.developertest.service
+
+import de.planerio.developertest.UnitTest
+import org.junit.experimental.categories.Category
+import spock.lang.Specification
+
+@Category(UnitTest.class)
+class CountryServiceTest extends Specification {
+
+   def "my first test"(){
+   }
+}
