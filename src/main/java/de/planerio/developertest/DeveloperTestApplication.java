912c29dbdca121f7b72fefc511fadae783450954
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2020-03-22 19:30:58 -0300

    refactor: cleanup

--- src/main/java/de/planerio/developertest/DeveloperTestApplication.java
+++ src/main/java/de/planerio/developertest/DeveloperTestApplication.java
@@ -2,12 +2,10 @@ package de.planerio.developertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("de.planerio.developertest")
-@EntityScan("de.planerio.developertest")
 @SpringBootApplication
 @ComponentScan(basePackages = { "de.planerio.developertest", "de.planerio.developertest.controller" , "de.planerio.developertest.config"})
 public class DeveloperTestApplication {
